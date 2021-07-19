e9bd11f4e0f1e9f96f5f236f9301739a8ad68a01
Mario Osorio Urias <mario.osoriourias@gmail.com>
2021-07-19 17:38:34 -0600

    ultimos cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Consultorio') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <link rel="shortcut icon" type="image/png" href="{{ asset('/img/favicon.jpg') }}">
+        <link rel="shortcut icon" sizes="192x192" href="{{ asset('/img/favicon.jpg') }}">
         
         @livewireStyles
 
